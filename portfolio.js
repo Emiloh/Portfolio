33c58e03c44d546e017bf13557aefbd31bca8964
Emiloh <74539948+Emiloh@users.noreply.github.com>
2020-12-07 15:49:03 +0100

    Ny menu. Ændrede i min tekst

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,6 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+function openNav() {
+  document.getElementById("mylinks").style.width = "190px";
+}  
+function closeNav() {
+  document.getElementById("mylinks").style.width = "0";
+}
